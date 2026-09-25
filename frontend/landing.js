a0843ca0b897cc0fc6d447637dd7613e7b8c586d
Muneeswar Reddy <muni477@gmail.com>
2026-09-25 20:58:24 +0530

    Fix mobile landing page navigation

---
+++ frontend/landing.js
@@ -0,0 +1,6 @@
+const pageSelector = document.getElementById('pageSelector');
+
+pageSelector?.addEventListener('change', event => {
+  if (event.target.value === 'ganesh') window.location.assign('/ganesh-public.html');
+  if (event.target.value === 'family') window.location.assign('/family.html');
+});
